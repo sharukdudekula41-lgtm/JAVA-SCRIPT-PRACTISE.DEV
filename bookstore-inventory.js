8fe6157b6d1d6cf234a063c932a487d8f28e18e8
Dudekula Sharukh <sharukdudekula41@gmail.com>
2026-08-01 23:12:11 +0530

    Add files via upload

---
+++ bookstore-inventory.js
@@ -0,0 +1,17 @@
+// 1. Basic Variables
+const username = "DevUser";   // String
+let loginCount = 3;           // Number
+const isAdmin = true;         // Boolean
+const num1 = 10;
+const num2 = 20;
+
+// 2. Data Structures
+const profile = { id: 101, bio: "Developer" }; // Object
+const skills = ["JavaScript", "HTML", "CSS"];    // Array
+// Adding the two numbers
+const sum = num1 + num2;
+
+console.log(`User: ${username} (Logins: ${loginCount})`);
+console.log(`Admin Status: ${isAdmin}`);
+console.log(`Primary Skill: ${skills[0]}`);
+console.log(`The sum of ${num1} and ${num2} is: ${sum}`);
